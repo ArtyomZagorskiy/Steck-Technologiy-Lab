9dc9f97d30495a46016b01aaac4d836300e16c05
Artyom Zagorskiy <108829302+ArtyomZagorskiy@users.noreply.github.com>
2024-10-22 16:34:04 +0200

    Lab 1

---
+++ Lab 1 Banking App/BankLibrary/Account.cs
@@ -0,0 +1,56 @@
+﻿namespace BankLibrary
+{
+    public class Account
+    {
+        public string cardNumber;
+        public int pin;
+        public string name;
+        public float money;
+
+        public Account(string _cardNumber,int _pin, string _name, float _money)
+        {
+            cardNumber = _cardNumber;
+            pin = _pin;
+            name = _name;
+            money = _money;
+        }
+
+        public void Withdraw(float amount)
+        {
+            if(money >= amount)
+            {
+                money -= amount;
+            }
+        }
+
+        public void Topup(float amount)
+        {
+            if (amount > 0)
+            {
+                money += amount;
+            }
+        }
+
+        public float getCurrentMoneyNumber()
+        {
+            return money;
+        }
+
+        public void setMoneyNumber(float newAmount)
+        {
+            money = newAmount;
+        }
+
+        public bool pinChecker(int _pin)
+        {
+            return pin == _pin;
+        }
+
+        public bool cardNumberCheker(string _cardNumber)
+        {
+            return cardNumber == _cardNumber;
+        }
+
+        
+    }
+}
---
+++ Lab 1 Banking App/BankLibrary/Bank.cs
@@ -0,0 +1,51 @@
+﻿namespace BankLibrary
+{
+    public class Bank
+    {
+        public string name;
+        public List<AutomatedTellerMachine> machines = new List<AutomatedTellerMachine>();
+        public List<Account> accounts = new List<Account>();
+        
+        public Account GetAccountByCardNumber(string cardNumber)
+        {
+            if (accounts == null)
+                return null;
+            
+            for(int i = 0; i < accounts.Count; i++)
+            {
+                if(accounts[i].cardNumberCheker(cardNumber))
+                {
+                    return accounts[i];
+                }
+            }
+
+            return null;
+        }
+
+        public void AddNewAccount(string _cardNumber, int _pin, string _name, float _money)
+        {
+            Account newAccount = new Account(_cardNumber, _pin, _name, _money);
+
+            accounts.Add(newAccount);   
+        }
+
+        public void AddNewATM(float moneyCount, int id, Bank bank)
+        {
+            AutomatedTellerMachine machine = new AutomatedTellerMachine(moneyCount, id, bank);
+
+            machines.Add(machine);
+        }
+
+        public bool isCardExist(string _cardNumber)
+        {
+            for(int i = 0; i < accounts.Count; i++)
+            {
+                if (accounts[i].cardNumberCheker(_cardNumber))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
